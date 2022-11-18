7acbae182d1804516ea318fc26f71f72dca56e13
minolinpwork <minolinp.work@gmail.com>
2022-11-18 16:27:09 +0200

    Working lotto - only correct numbers

---
+++ app/plutus-lotto.hs
@@ -0,0 +1,37 @@
+import Prelude
+
+import Cardano.Api
+import           Cardano.Api.Shelley
+
+import System.Directory
+import System.FilePath.Posix ((</>))
+
+import Plutus.Contracts.Lotto (gameScript, LockParams(..), GuessParams(..), hashString, clearString, HashedString, ClearString)
+
+import qualified Plutus.V1.Ledger.Api as Plutus
+import qualified Data.ByteString.Short as SBS
+import qualified Data.ByteString.Lazy  as LBS
+import           Data.Aeson (encode)
+import qualified Ledger.Ada            as Ada
+
+main :: IO ()
+main = do
+  let v1dir = "generated-plutus-scripts/v1"
+      v2dir = "generated-plutus-scripts/v2"
+  createDirectoryIfMissing True v1dir
+  createDirectoryIfMissing True v2dir
+
+
+  let hashedStringPass = hashString "HelloWorld"
+      clearStringPass = clearString "HelloWorld"
+      clearStringFail = clearString "Failling"
+      lockParams = LockParams "HelloWorld" (Ada.adaValueOf 10)
+      guessParams = GuessParams "HelloWorld"
+  LBS.writeFile (v1dir </> "result.datum") $ encode (scriptDataToJson ScriptDataJsonDetailedSchema $ fromPlutusData (Plutus.toData hashedStringPass))
+  LBS.writeFile (v1dir </> "result.redeem") $ encode (scriptDataToJson ScriptDataJsonDetailedSchema $ fromPlutusData (Plutus.toData clearStringPass))
+  LBS.writeFile (v1dir </> "result.fail.redeem") $ encode (scriptDataToJson ScriptDataJsonDetailedSchema $ fromPlutusData (Plutus.toData clearStringFail))
+  result <- writeFileTextEnvelope (v1dir </> "result.plutus") Nothing gameScript
+
+  case result of
+    Left err -> print $ displayError err
+    Right () -> return ()
